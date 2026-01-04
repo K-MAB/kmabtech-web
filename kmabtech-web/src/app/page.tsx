4e2171914b64d9a4acd70412165769b128c938ff
yucel.kandas <yucel_kandas@hotmail.com>
2026-01-04 13:12:56 +0300

    ty

--- kmabtech-web/src/app/page.tsx
+++ kmabtech-web/src/app/page.tsx
@@ -52,7 +52,6 @@ export default function HomePage() {
                   <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-green-400 opacity-75"></span>
                   <span className="relative inline-flex rounded-full h-2 w-2 bg-green-500"></span>
                 </span>
-                <span className="text-xs font-mono text-gray-400">System Online v2.4</span>
               </motion.div>
 
               <motion.h1 
